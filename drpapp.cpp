d114c8369771eaf33b7bdbf98bcf410a0f7a1bcd
n0umen0n <46645013+n0umen0n@users.noreply.github.com>
2023-10-22 14:07:05 +0300

    new ACTIONS/TABLES

--- drpapp.cpp
+++ drpapp.cpp
@@ -7,46 +7,242 @@ drpapp::drpapp(
 ) : contract(self, code, ds)
 {}
 
+
+void drpapp::createcase(name community, name claimant_name, uint64_t number, uint8_t nr_of_requested_arbitrators,
+    string case_description, map<name, uint8_t> arbitrators, vector<string> claims, vector<asset> fine, vector<asset> relief, 
+    vector<uint16_t> suspension, bool request_ban, string claimants_evidence_description, vector<string> claimants_ipfs_cids,
+    asset claimants_deposit, bool claimants_requested_deposit, map<name, string> claimants_socials, map<name, string> respondents_socials,
+    name respondents_account, string other_info_about_respondent)
+ {
+    require_auth(claimant_name); // Assuming the claimant initiates this action
+    check(is_account(respondents_account), "Respondent's account does not exist.");
+    // Checks for string lengths
+    check(case_description.size() <= 2000, "Case description exceeds 2000 characters.");
+    check(claimants_evidence_description.size() <= 2000, "Claimant's evidence description exceeds 2000 characters.");
+    check(other_info_about_respondent.size() <= 2000, "Other info about respondent exceeds 2000 characters.");
+
+    // Check the string length for each claim and each IPFS CID
+    for(const auto& claim : claims) 
+    {
+        check(claim.size() <= 400, "A claim description exceeds characters.");
+    }
+
+    for(const auto& cid : claimants_ipfs_cids) 
+    {
+        check(cid.size() <= 46, "A claimant IPFS CID exceeds characters.");
+    }
+
+    // Check the string lengths for socials 
+    for(const auto& [name, social]: claimants_socials) 
+    {
+        check(social.size() <= 100, "A claimant's social link exceeds 2000 characters.");
+    }
+
+    for(const auto& [name, social]: respondents_socials) 
+    {
+        check(social.size() <= 100, "A respondent's social link exceeds 2000 characters.");
+    }
+
+
+    action(
+        permission_level{get_self(), "active"_n},
+        "r4ndomnumb3r"_n,
+        "generate"_n,
+        std::make_tuple()
+    ).send();
+
+    arbitrators_t arbs_table(get_self(), community.value);
+    vector<name> all_arbitrators;
+
+    for (auto arb_itr = arbs_table.begin(); arb_itr != arbs_table.end(); ++arb_itr) {
+        all_arbitrators.push_back(arb_itr->arbitrator);
+    }
+
+    rng_t rndnmbr("r4ndomnumb3r"_n, "r4ndomnumb3r"_n.value);
+    checksum256 x = rndnmbr.get().value;
+    uint32_t seed = *reinterpret_cast<uint32_t*>(&x);
+    my_shuffle(all_arbitrators.begin(), all_arbitrators.end(), seed);
+
+    map<name, uint8_t> selected_arbitrators;
+    for (int i = 0; i < nr_of_requested_arbitrators && i < all_arbitrators.size(); i++) 
+    {
+      selected_arbitrators[all_arbitrators[i]] = 0;
+    } 
+
+    cases_t cases_table(get_self(), community.value);
+    cases_table.emplace(_self, [&](auto& row) {
+        row.case_id = cases_table.available_primary_key();
+        row.claimant_name = claimant_name;
+        row.number = number;
+        row.stage = 1;
+        row.nr_of_requested_arbitrators = nr_of_requested_arbitrators;
+        row.case_description = case_description;
+        row.arbitrators = selected_arbitrators;
+        row.claims = claims;
+        row.fine = fine;
+        row.relief = relief;
+        row.suspension = suspension;
+        row.request_ban = request_ban;
+        row.claimants_evidence_description = claimants_evidence_description;
+        row.claimants_ipfs_cids = claimants_ipfs_cids;
+        row.claimants_deposit = claimants_deposit;
+        row.claimants_requested_deposit = claimants_requested_deposit;
+        row.claimants_socials = claimants_socials;
+        row.respondents_socials = respondents_socials;
+        row.respondents_account = respondents_account;
+        row.other_info_about_respondent = other_info_about_respondent;
+    });
+}
+
+void drpapp::addarbs(name community, vector<name> arbitrator_names) 
+{
+    require_auth(community);  // Assuming the community itself or a privileged account should do this. Adjust as necessary.
+
+    // Check if the community exists in the config table
+    config_t config_table(get_self(), get_self().value);
+    auto config_itr = config_table.find(community.value);
+    check(config_itr != config_table.end(), "Community does not exist in the config table!");
+
+    arbitrators_t arbs(get_self(), community.value);  // Setting the scope to the community name
+
+    for (const auto& arb_name : arbitrator_names) 
+    {
+        auto itr = arbs.find(arb_name.value);
+        eosio::check(itr == arbs.end(), "Arbitrator already exists!");
+
+        arbs.emplace(get_self(), [&](auto& new_arb) {
+            new_arb.arbitrator = arb_name;
+        });
+    }
+}
+
+void drpapp::delarbs(name community, vector<name> arbitrator_names) 
+{
+    require_auth(community);  // Assuming the community itself or a privileged account should do this. Adjust as necessary.
+
+    arbitrators_t arbs(get_self(), community.value);  // Setting the scope to the community name
+
+    for (const auto& arb_name : arbitrator_names) 
+    {
+        auto itr = arbs.find(arb_name.value);
+        eosio::check(itr != arbs.end(), "Arbitrator not found!");
+
+        arbs.erase(itr);
+    }
+}
+
+void drpapp::acceptarbtrn(name arbitrator, uint64_t case_id, name community) 
+{
+    require_auth(arbitrator);  // Assuming the arbitrator themselves should do this. Adjust as necessary.
+
+    cases_t cases_table(get_self(), community.value); // Set the scope to the community name
+
+    // Find the case based on case_id
+    auto case_itr = cases_table.find(case_id);
+    check(case_itr != cases_table.end(), "Case does not exist!");
+
+    // Check if the arbitrator exists in the map
+    auto arb_itr = case_itr->arbitrators.find(arbitrator);
+    check(arb_itr != case_itr->arbitrators.end(), "Arbitrator is not associated with this case!");
+
+    // Modify the case to set the uint8_t value for this arbitrator to 1
+    cases_table.modify(case_itr, get_self(), [&](auto& row) {
+        row.arbitrators[arbitrator] = 1;
+    });
+}
+
+void drpapp::rejectarbtrn(name arbitrator, uint64_t case_id, name community) 
+{
+    require_auth(arbitrator);  // Assuming the arbitrator themselves should do this. Adjust as necessary.
+
+    cases_t cases_table(get_self(), community.value); // Set the scope to the community name
+
+    // Find the case based on case_id
+    auto case_itr = cases_table.find(case_id);
+    check(case_itr != cases_table.end(), "Case does not exist!");
+
+    // Check if the arbitrator exists in the map
+    auto arb_itr = case_itr->arbitrators.find(arbitrator);
+    check(arb_itr != case_itr->arbitrators.end(), "Arbitrator is not associated with this case!");
+
+    // Fetch all available arbitrators
+    arbitrators_t arbs_table(get_self(), community.value);
+    vector<name> all_arbitrators;
+    for (auto itr = arbs_table.begin(); itr != arbs_table.end(); ++itr) {
+        all_arbitrators.push_back(itr->arbitrator);
+    }
+
+    // Shuffle to get a random order
+    rng_t rndnmbr("r4ndomnumb3r"_n, "r4ndomnumb3r"_n.value);
+    checksum256 x = rndnmbr.get().value;
+    uint32_t seed = *reinterpret_cast<uint32_t*>(&x);
+    my_shuffle(all_arbitrators.begin(), all_arbitrators.end(), seed);
+
+    // Find a new arbitrator that is not in the current arbitrators map
+    name new_arbitrator;
+    for (auto& arb_name : all_arbitrators) {
+        if (case_itr->arbitrators.find(arb_name) == case_itr->arbitrators.end()) {
+            new_arbitrator = arb_name;
+            break;
+        }
+    }
+    check(is_account(new_arbitrator), "Failed to find a replacement arbitrator.");
+
+    // Modify the case: remove the rejecting arbitrator and add the new arbitrator
+    cases_table.modify(case_itr, get_self(), [&](auto& row) {
+        row.arbitrators.erase(arbitrator);
+        row.arbitrators[new_arbitrator] = 0;
+    });
+}
+
+
+
 void drpapp::addcase(name community) 
 {
-cases_t cases(_self, community.value);
+    cases_t cases(_self, community.value);
 
     cases.emplace(_self, [&](auto& row) {
-        row.claimant_name = "deftclaimnt"_n; 
+        row.claimant_name = "kyotokimura2"_n; 
         row.case_id = cases.available_primary_key();
         row.number = 0;
         row.stage = 1;
         row.nr_of_requested_arbitrators = 1;
         row.case_description = "Default case description.";
-        row.arbitrators = {{"arbitrator1"_n, 0}, {"arbitrator2"_n, 1}};
-        row.claims = {"Default claim"};
-        row.fine = {asset(10000, symbol("EOS", 4))};  // 1.0000 EOS as default fine
-        row.relief = {asset(5000, symbol("EOS", 4))}; // 0.5000 EOS as default relief
-        row.suspension = {1};
+        row.arbitrators = {{"vladislav.x"_n, 0}, {"lennyaccount"_n, 1}};
+        
+        // Adding multiple default values
+        row.claims = {"Default claim 1", "Default claim 2", "Default claim 3"};
+        row.fine = {asset(10000, symbol("EOS", 4)), asset(9000, symbol("EOS", 4)), asset(8000, symbol("EOS", 4))};
+        row.relief = {asset(5000, symbol("EOS", 4)), asset(4000, symbol("EOS", 4)), asset(3000, symbol("EOS", 4))};
+        row.suspension = {1, 2, 3};
+        
         row.request_ban = false;
         row.claimants_evidence_description = "Default evidence description.";
         row.claimants_ipfs_cids = {"QmDefaultCID1", "QmDefaultCID2"};
-        row.claimants_deposit = asset(20000, symbol("EOS", 4)); // 2.0000 EOS
+        row.claimants_deposit = asset(20000, symbol("EOS", 4));
         row.claimants_requested_deposit = false;
-        row.claimants_telegram = "@defaultClaimant";
-        row.respondents_telegram = "@defaultRespondent";
-        row.respondents_account = "defaultresp"_n;
+
+        // Setting the claimants and respondents Telegram account names
+        row.claimants_socials = {{"telegram"_n, "@defaultClaimant"}};
+        row.respondents_socials = {{"telegram"_n, "@defaultRespondent"}};
+
+        row.respondents_account = "ironscimitar"_n;
         row.other_info_about_respondent = "Default respondent info.";
         row.accusations_accepted_by_respondent = false;
         row.respondents_response = "Default response.";
         row.respondents_ipfs_cids = {"QmDefaultRespCID1"};
         row.respondents_evidents_description = "Default respondent evidence description.";
-        row.fine_counter = {asset(500, symbol("EOS", 4))}; // 0.0500 EOS
-        row.relief_counter = {asset(250, symbol("EOS", 4))}; // 0.0250 EOS
-        row.suspension_counter = {2};
-        row.respondent_deposit = asset(1000, symbol("EOS", 4)); // 0.1000 EOS
+        row.fine_counter = {asset(500, symbol("EOS", 4)), asset(0, symbol("EOS", 0)), asset(400, symbol("EOS", 4))};
+        row.relief_counter = {asset(250, symbol("EOS", 4)), asset(225, symbol("EOS", 0)), asset(200, symbol("EOS", 4))};
+        row.suspension_counter = {2, 0, 4};
+        row.respondent_deposit = asset(1000, symbol("EOS", 4));
         row.respondent_requested_deposit = false;
         row.case_winner = "defaultwinr"_n;
-        row.fine_verdict = {asset(300, symbol("EOS", 4))}; // 0.0300 EOS
-        row.relief_verdict = {asset(150, symbol("EOS", 4))}; // 0.0150 EOS
-        row.suspension_verdict = {3};
+        row.fine_verdict = {asset(300, symbol("EOS", 4)), asset(290, symbol("EOS", 4)), asset(280, symbol("EOS", 4))};
+        row.relief_verdict = {asset(150, symbol("EOS", 4)), asset(145, symbol("EOS", 4)), asset(140, symbol("EOS", 4))};
+        row.suspension_verdict = {3, 4, 5};
         row.verdict_description = "Default verdict description.";
-        row.arbitrator_and_signatures = {{"arbitrator1"_n, 1}, {"arbitrator2"_n, 0}};
+        row.arbitrator_and_signatures = {{"vladislav.x"_n, 1}, {"lennyaccount"_n, 0}};
         row.ipfs_cid_verdict = {"QmVerdictCID"};
     });
 }
@@ -71,6 +267,16 @@ void drpapp::addconfig(name community)
         row.time_for_respondent_to_respond_the_case = 172800; // 48 hours (in seconds)
     });
 
+}
+
+void drpapp::delcase(name community, uint64_t case_id) 
+{
+    cases_t cases(_self, community.value);
+    auto itr = cases.find(case_id);
+
+    eosio::check(itr != cases.end(), "Case with specified case_id not found.");
+
+    cases.erase(itr);
 }
 
 
--- drpapp.hpp
+++ drpapp.hpp
@@ -12,12 +12,28 @@
 using namespace eosio;
 using namespace std;
 
+CONTRACT r4ndomnumb3r : public contract
+{
+public:
+    using contract::contract;
+
+    ACTION generate();
+    using generate_action = action_wrapper<"generate"_n, &r4ndomnumb3r::generate>;
+};
+
 
 CONTRACT drpapp : public contract
 {
 public:
 
-    TABLE cases {
+    TABLE rng
+    {
+        checksum256 value;
+    };
+    using rng_t = singleton<"rng"_n, rng>;
+
+    TABLE cases 
+    {
         name claimant_name;
         uint64_t case_id;
         uint64_t number;
@@ -34,8 +50,8 @@ public:
         vector<string> claimants_ipfs_cids;
         asset claimants_deposit;
         bool claimants_requested_deposit;
-        string claimants_telegram;
-        string respondents_telegram;
+        map<name, string> claimants_socials;
+        map<name, string> respondents_socials;
         name respondents_account;
         string other_info_about_respondent;
         bool accusations_accepted_by_respondent;
@@ -61,10 +77,8 @@ public:
     typedef eosio::multi_index<"cases"_n, cases,
     indexed_by<"bynumber"_n, const_mem_fun<cases, uint64_t, &cases::get_by_number>>> cases_t;
 
-
-
-
-    TABLE config {
+    TABLE config 
+    {
         name community;
         string community_name;
         string community_description;
@@ -82,15 +96,79 @@ public:
     };
     typedef eosio::multi_index<"config"_n, config> config_t;
 
+    TABLE communities 
+    {
+        name community;
+        uint64_t primary_key() const { return community.value; }
+    };
+    typedef eosio::multi_index<"communities"_n, communities> communities_t;
+
+    TABLE arbitrators {
+        name arbitrator;
+        uint64_t primary_key() const { return arbitrator.value; }
+    };
+    typedef eosio::multi_index<"arbitrators"_n, arbitrators> arbitrators_t;
+
 
     drpapp(name self, name code, datastream<const char *> ds);
 
+    ACTION createcase();
     ACTION addcase(name community);
     ACTION addconfig(name community);
-  
-private:
+    ACTION delcase(name community,uint64_t case_id);
+    ACTION addarbs(name community, vector<name> arbitrator_names);
+    ACTION delarbs(name community, vector<name> arbitrator_names);
+    ACTION createcase(name community, name claimant_name, uint64_t number, uint8_t nr_of_requested_arbitrators, string case_description, map<name, uint8_t> arbitrators, vector<string> claims, vector<asset> fine, vector<asset> relief, vector<uint16_t> suspension, bool request_ban, string claimants_evidence_description, vector<string> claimants_ipfs_cids, asset claimants_deposit, bool claimants_requested_deposit, map<name, string> claimants_socials, map<name, string> respondents_socials, name respondents_account, string other_info_about_respondent);
+    ACTION acceptarbtrn(name arbitrator, uint64_t case_id, name community);
+    
 
-void require_auth_either(name user1, name user2);
+
+
+    private:
+
+    void require_auth_either(name user1, name user2);
+
+    // simple generic swap
+    template<typename T>
+    void my_swap(T& t1, T& t2)
+    {
+        T tmp = move(t1);
+        t1 = move(t2);
+        t2 = move(tmp);
+    }
+
+    // an adaption of: https://cplusplus.com/reference/algorithm/shuffle/
+    template<class RandomAccessIterator>
+    void my_shuffle(RandomAccessIterator first, RandomAccessIterator last, uint32_t seed)
+    {
+        for(auto i = (last - first) - 1; i > 0; --i)
+        {
+            uint32_t r = seed % (i+1);
+            my_swap(first[i], first[r]);
+            // source: https://stackoverflow.com/a/11946674/2340535
+            seed = (seed * 1103515245U + 12345U) & 0x7fffffffU;
+        }
+    }
+
+    // a simple generic "shell sort" implementation
+    // borrowed from: https://www.softwaretestinghelp.com/sorting-techniques-in-cpp/#Shell_Sort
+    template <typename T, typename F>
+    void my_sort(T arr[], int n, F predicate)
+    {
+        for(int gap = n/2; gap > 0; gap /= 2)
+        {
+            for(int i = gap; i < n; i += 1)
+            {
+                T tmp = arr[i];
+                int j;
+                for(j = i; j >= gap && predicate(tmp, arr[j - gap]); j -= gap)
+                {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = tmp;
+            }
+        }
+    }
 
 
 };
